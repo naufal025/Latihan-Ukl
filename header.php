b5d211ed9f6a6e7b5c1b9b37fc3d36b56a0e7e02
naufal025 <53498520+naufal025@users.noreply.github.com>
2020-03-29 10:23:30 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/XR2/siperpus/aset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/XR2/siperpus/aset/fontawesome-free-5.13.0-web/css/all.min.css">
+
+    <title>SiPERPUS</title>
+</head>
+<body>
+  
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+<a class="nav-item nav-link" href="http://localhost/siperpus/index.php">Dashboard</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">Peminjaman</a>
+<a class="nav-item nav-link" href="#">Logout</a>
+
+        </div>
+    </div>
+        </a>
+
+
+    </form>
+  </div>
+</nav>
+</body>
+</html>
